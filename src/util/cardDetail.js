29e324876adb188f880df55afc18a687fd2d9350
Bruno Volpe <bruno@Brunos-MacBook-Pro.local>
2023-10-06 20:26:57 -0300

    update CardDetail

--- src/util/cardDetail.js
+++ src/util/cardDetail.js
@@ -31,18 +31,6 @@ class CardDetail {
         }
     }
 
-    checkVariableCount() {
-        const regex = /n\d+/g;
-        const matches = this.latexFormula.match(regex);
-
-        if (matches) {
-            const uniqueMatches = [...new Set(matches)]; // Remove duplicatas
-            return uniqueMatches.length === Number(this.numberOfGrades);
-        }
-
-        return false;
-    }
-
     getStatusMedia() {
         const mediaAluno = this.resolveFormula()
         if (mediaAluno >= this.media) {
@@ -52,71 +40,27 @@ class CardDetail {
     }
 
     calculateMissingVariables() {
-        const regex = /n\d+/g;
-        const matches = this.latexFormula.match(regex);
 
-        if (matches) {
-            const uniqueMatches = [...new Set(matches)]; // Remove duplicatas
-            const missingVariables = [];
+        const newValues = this.values.map((value, i) => {
+            return typeof value.nota === 'number' ? value.nota : `n${i + 1}`;
+        });
 
-            for (let i = 1; i <= this.numberOfGrades; i++) {
-                if (!uniqueMatches.includes(`n${i}`)) {
-                    missingVariables.push(i);
+        const equation = `(${newValues.join(' + ')}) / ${newValues.length} = ${this.media}`;
+
+        try {
+            const solution = evaluate(equation);
+
+            const updatedValues = this.values.map((value, i) => {
+                if (typeof value.nota !== 'number') {
+                    return { ...value, nota: solution[i] };
                 }
-            }
+                return value;
+            });
 
-            if (missingVariables.length > 0) {
-                const possibleValues = [];
-
-                for (let i = 1; i <= this.numberOfGrades; i++) {
-                    if (!this.values.find(value => value.nota === i)) {
-                        possibleValues.push(i);
-                    }
-                }
-
-                if (possibleValues.length >= missingVariables.length) {
-                    const combinations = this.generateCombinations(possibleValues, missingVariables.length);
-
-                    for (const combination of combinations) {
-                        const tempValues = [...this.values];
-                        for (let i = 0; i < missingVariables.length; i++) {
-                            const index = tempValues.findIndex(value => value.nota === combination[i]);
-                            tempValues[index] = { nota: missingVariables[i] };
-                        }
-
-                        const cardDetail = new CardDetail(this.latexFormula, tempValues, this.numberOfGrades, this.media);
-                        const result = cardDetail.resolveFormula();
-
-                        if (result === this.media) {
-                            return tempValues;
-                        }
-                    }
-                }
-            }
+            return ({ values: updatedValues });
+        } catch (error) {
+            console.error("Erro ao resolver a equação:", error);
         }
-
-        return null;
-    }
-
-    generateCombinations(arr, size) {
-        const combinations = [];
-
-        function backtrack(start, combination) {
-            if (combination.length === size) {
-                combinations.push([...combination]);
-                return;
-            }
-
-            for (let i = start; i < arr.length; i++) {
-                combination.push(arr[i]);
-                backtrack(i + 1, combination);
-                combination.pop();
-            }
-        }
-
-        backtrack(0, []);
-
-        return combinations;
     }
 }
 
