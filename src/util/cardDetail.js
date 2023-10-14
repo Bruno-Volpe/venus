9ff2f48b66f4e9a11b6c295d1a95b2a5d957c587
Bruno Volpe <bruno@Brunos-MacBook-Pro.local>
2023-10-14 19:44:37 -0300

    fix n(s) problem

--- src/util/cardDetail.js
+++ src/util/cardDetail.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { simplify } from 'mathjs';
+//import { simplify } from 'mathjs';
 
 class CardDetail {
     constructor(
@@ -36,11 +36,14 @@ class CardDetail {
     checkVariableCount() {
         const regex = /n\d+/g;
         const matches = this.latexFormula.match(regex);
+        const numberOfGrades = criarArray(this.numberOfGrades).map(n => `n${n}`);
 
         if (matches) {
             const uniqueMatches = [...new Set(matches)]; // Remove duplicatas
 
-            return uniqueMatches.length === Number(this.numberOfGrades);
+            if (uniqueMatches.length === Number(this.numberOfGrades)) {
+                return isEqualArray(uniqueMatches, numberOfGrades);
+            };
         }
 
         return false;
@@ -68,7 +71,7 @@ class CardDetail {
                 .replace(/\\frac{(\d+)}{(\d+)}/g, '$1 / $2')
                 .replace(/\\sqrt{([^}]*)}/g, 'sqrt($1)');
 
-            formula = simplify(`${formula.replace(/n(\d+)/g, 'n')}`).toString();
+            formula = (`${formula.replace(/n(\d+)/g, 'n')}`) //PODE DAR ERRO AQUI
             return resolverEquacao(formula, this.media).then((result) => {
                 return (result.filter(elemento => elemento > 0)[0])
             })
@@ -76,6 +79,32 @@ class CardDetail {
             return 'Erro na expressão';
         }
     }
+}
+
+function isEqualArray(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    const sortedArr1 = arr1.slice().sort();
+    const sortedArr2 = arr2.slice().sort();
+
+    for (let i = 0; i < sortedArr1.length; i++) {
+        if (sortedArr1[i] !== sortedArr2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+function criarArray(numero) {
+    let resultado = [];
+    for (let i = 1; i <= numero; i++) {
+        resultado.push(i);
+    }
+    return resultado;
 }
 
 async function resolverEquacao(expressao, valor) {
