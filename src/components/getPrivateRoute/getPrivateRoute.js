d9f6162c05be2461c572769b108a785cf81331e1
Bruno Volpe <bruno@Brunos-MacBook-Pro.local>
2023-10-15 11:06:35 -0300

    refactorinting privates routes

--- src/components/getPrivateRoute/getPrivateRoute.js
+++ src/components/getPrivateRoute/getPrivateRoute.js
@@ -1,7 +1,53 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Route } from "react-router-dom"
-import PrivateRoute from './PrivateRoute';
 
+import { auth } from '../../service/firebaseConnection'
+import { onAuthStateChanged } from 'firebase/auth'
+
+import { Navigate } from 'react-router-dom'
+
+function PrivateRoute({ children }) {
+    const [loading, setLoading] = useState(true);
+    const [signed, setSigned] = useState(false);
+
+    useEffect(() => {
+        async function checkLogin() {
+            onAuthStateChanged(auth, (user) => {
+                //se tem user logado
+                if (user) {
+                    const userData = {
+                        uid: user.uid,
+                        email: user.email,
+                    }
+
+                    localStorage.setItem("@detailUser", JSON.stringify(userData))
+
+                    setLoading(false);
+                    setSigned(true);
+
+                } else {
+                    //nao possui user logado
+                    setLoading(false);
+                    setSigned(false);
+                }
+            })
+        }
+
+        checkLogin();
+    }, [])
+
+    if (loading) {
+        return (
+            <div></div>
+        )
+    }
+
+    if (!signed) {
+        return <Navigate to="/login" />
+    }
+
+    return children;
+}
 
 const getPrivateRoute = (index, path, Component) => {
     return (
